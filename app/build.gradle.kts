35e72857e0a4f01271e72157c21395a4e6055947
github-mathieu-capo <mathieu.capo@teads.tv>
2024-01-25 10:39:49 +0100

    retrieve image from firebase and show it on the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,16 +28,16 @@ android {
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 dependencies {
-
+    implementation("com.firebaseui:firebase-ui-storage:8.0.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.google.firebase:firebase-storage")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-
     implementation("com.google.firebase:firebase-analytics")
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/helloandroid/ut3/miniprojet2024android/MainActivity.java
+++ app/src/main/java/helloandroid/ut3/miniprojet2024android/MainActivity.java
@@ -1,14 +1,54 @@
 package helloandroid.ut3.miniprojet2024android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        // Firebase Storage references
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference imageRef = storageRef.child("uploaded_images/zebanane.jpg");
+        ImageView imageView = findViewById(R.id.imageView);
+
+        loadImageFromStorageReference(imageRef, imageView);
     }
+    private void loadImageFromStorageReference(StorageReference storageReference, ImageView imageView) {
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String downloadUrl = uri.toString();
+
+                Glide.with(getApplicationContext())
+                        .load(downloadUrl)
+                        .into(imageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.e("TAG", "Cannot retrieve Image", exception);
+            }
+        });
+    }
+
 }
