1c93294f77b42c2118a32d33457dfe670e28ef49
github-mathieu-capo <mathieu.capo@teads.tv>
2024-01-25 11:18:45 +0100

    list restaurant

---
+++ app/src/main/java/helloandroid/ut3/miniprojet2024android/RestaurantAdapter.java
@@ -0,0 +1,61 @@
+package helloandroid.ut3.miniprojet2024android;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import helloandroid.ut3.miniprojet2024android.model.Restaurant;
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> {
+
+    private List<Restaurant> restaurantList;
+    private Context context;
+
+    public RestaurantAdapter(List<Restaurant> restaurantList, Context context) {
+        this.restaurantList = restaurantList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurants, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Restaurant restaurant = restaurantList.get(position);
+        holder.imageView.setImageResource(restaurant.getImageResourceId());
+        holder.textViewName.setText(restaurant.getName());
+        holder.textViewDescription.setText(restaurant.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return restaurantList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textViewName;
+        TextView textViewDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.restaurantImage);
+            textViewName = itemView.findViewById(R.id.restaurantName);
+            textViewDescription = itemView.findViewById(R.id.restaurantDescription);
+
+        }
+    }
+}
+
---
+++ app/src/main/java/helloandroid/ut3/miniprojet2024android/utilities/FireBaseImageUploader.java
@@ -0,0 +1,5 @@
+package helloandroid.ut3.miniprojet2024android.utilities;
+
+public class FireBaseImageUploader {
+
+}
